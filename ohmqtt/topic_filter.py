a183e1958231d49194e90ea7b7363957de469391
Matt Vollrath <matt@endpointdev.com>
2025-04-01 16:03:41 -0400

    Add MQTTTopicFilter

---
+++ ohmqtt/topic_filter.py
@@ -0,0 +1,56 @@
+class MQTTTopicFilter:
+    """A topic filter for MQTT."""
+    __slots__ = ("_topic_filter",)
+
+    def __init__(self, topic_filter: str):
+        if len(topic_filter) == 0:
+            raise ValueError("Topic filter cannot be empty")
+        if "\u0000" in topic_filter:
+            raise ValueError("Topic filter cannot contain null characters")
+        if len(topic_filter.encode("utf-8")) > 65535:
+            raise ValueError("Topic filter is too long (> 65535 bytes encoded as UTF-8)")
+        multi_level_wildcard_index = topic_filter.find("#")
+        if multi_level_wildcard_index != -1:
+            if multi_level_wildcard_index != len(topic_filter) - 1:
+                raise ValueError("Multi-level wildcard '#' must be the last character in the topic filter")
+            if len(topic_filter) > 1 and topic_filter[-2] != "/":
+                raise ValueError("Multi-level wildcard '#' must be preceded by a '/' unless it is the only character in the topic filter")
+        self._topic_filter = topic_filter
+
+    @property
+    def topic_filter(self) -> str:
+        """Get the topic filter."""
+        return self._topic_filter
+
+    def match(self, topic: str) -> bool:
+        """Check if the topic matches the filter."""
+        if len(topic) == 0:
+            raise ValueError("Topic name cannot be empty")
+        if "\u0000" in topic:
+            raise ValueError("Topic name cannot contain null characters")
+        if "#" in topic or "+" in topic:
+            raise ValueError("Topic name cannot contain wildcards '#' or '+'")
+        if len(topic.encode("utf-8")) > 65535:
+            raise ValueError("Topic name is too long (> 65535 bytes encoded as UTF-8)")
+        if self._topic_filter == topic:
+            return True  # Exact match.
+        hidden_topic = topic.startswith("$")
+        if self._topic_filter == "#" and not hidden_topic:
+            return True  # Matches everything that doesn't start with '$'.
+        if "#" in self._topic_filter:
+            base = self._topic_filter[:-2]
+            if base and topic.startswith(base):
+                return True  # Matches everything under the base topic.
+            return False
+        if "+" in self._topic_filter:
+            if self._topic_filter.startswith("+") and hidden_topic:
+                return False
+            filter_levels = self._topic_filter.split("/")
+            topic_levels = topic.split("/")
+            if len(filter_levels) != len(topic_levels):
+                return False
+            for filter_level, topic_level in zip(filter_levels, topic_levels):
+                if filter_level != "+" and filter_level != topic_level:
+                    return False
+            return True
+        return False
---
+++ tests/test_topic.py
@@ -0,0 +1,94 @@
+import pytest
+
+from ohmqtt.topic_filter import MQTTTopicFilter
+
+
+def test_topic_filter_properties():
+    filter = MQTTTopicFilter("sport/tennis/player1")
+    assert filter.topic_filter == "sport/tennis/player1"
+
+def test_topic_filter_empty_filter():
+    with pytest.raises(ValueError):
+        MQTTTopicFilter("")
+
+def test_topic_filter_empty_topic():
+    filter = MQTTTopicFilter("sport/tennis/player1")
+    with pytest.raises(ValueError):
+        filter.match("")
+
+def test_topic_filter_null_character_filter():
+    with pytest.raises(ValueError):
+        MQTTTopicFilter("sport/tennis/\u0000player1")
+
+def test_topic_filter_null_character_topic():
+    filter = MQTTTopicFilter("sport/tennis/player1")
+    with pytest.raises(ValueError):
+        filter.match("sport/tennis/\u0000player1")
+
+def test_topic_filter_long_filter():
+    long_filter = "a" * 65536
+    with pytest.raises(ValueError):
+        MQTTTopicFilter(long_filter)
+
+def test_topic_filter_long_topic():
+    filter = MQTTTopicFilter("sport/tennis/player1")
+    long_topic = "a" * 65536
+    with pytest.raises(ValueError):
+        filter.match(long_topic)
+
+def test_topic_filter_exact_match():
+    filter = MQTTTopicFilter("sport/tennis/player1")
+    assert filter.match("sport/tennis/player1")
+    assert not filter.match("sport/tennis/player2")
+    assert not filter.match("sport/tennis")
+    assert not filter.match("sport")
+
+def test_topic_filter_multi_level_wildcard_match():
+    filter = MQTTTopicFilter("sport/tennis/player1/#")
+    assert filter.match("sport/tennis/player1")
+    assert filter.match("sport/tennis/player1/ranking")
+    assert filter.match("sport/tennis/player1/score/wimbledon")
+    assert not filter.match("sport/tennis/player2")
+    assert not filter.match("sport/tennis")
+    assert not filter.match("sport")
+
+def test_topic_filter_multi_level_wildcard_match_hidden():
+    filter = MQTTTopicFilter("$SYS/#")
+    assert filter.match("$SYS/monitor/Clients")
+
+def test_topic_filter_multi_level_wildcard_match_all():
+    filter = MQTTTopicFilter("#")
+    assert filter.match("sport/tennis/player1")
+    assert filter.match("sport/tennis/player1/ranking")
+    assert filter.match("sport/tennis/player1/score/wimbledon")
+    assert filter.match("sport/tennis/player2")
+    assert filter.match("sport/tennis")
+    assert filter.match("sport")
+    assert not filter.match("$SYS/monitor/Clients")
+
+def test_topic_filter_multi_level_wildcard_invalid_filter():
+    with pytest.raises(ValueError):
+        MQTTTopicFilter("sport/tennis#")
+    with pytest.raises(ValueError):
+        MQTTTopicFilter("sport/tennis/#/ranking")
+
+def test_topic_filter_multi_level_wildcard_invalid_topic():
+    filter = MQTTTopicFilter("sport/tennis/player1/#")
+    with pytest.raises(ValueError):
+        filter.match("sport/tennis/player1/#")
+
+def test_topic_filter_single_level_wildcard_match():
+    filter = MQTTTopicFilter("sport/tennis/+/ranking")
+    assert filter.match("sport/tennis/player1/ranking")
+    assert filter.match("sport/tennis/player2/ranking")
+    assert not filter.match("sport/tennis/player1")
+    assert not filter.match("sport/tennis/player1/score")
+    assert not filter.match("sport/tennis/player1/score/wimbledon")
+    assert not filter.match("sport/tennis/ranking")
+
+def test_topic_filter_single_level_wildcard_match_hidden():
+    filter = MQTTTopicFilter("$SYS/+/Clients")
+    assert filter.match("$SYS/monitor/Clients")
+
+    filter = MQTTTopicFilter("+/monitor/Clients")
+    assert not filter.match("$SYS/monitor/Clients")
