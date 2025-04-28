f8ef49a8a4107570729e896fc8f60ea05be4da6e
Matt Vollrath <matt@endpointdev.com>
2025-04-28 03:20:34 -0400

    Add RPC server examples

---
+++ examples/rpc_caller.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""This example demonstrates a simple RPC caller.
+
+It sends an RPC request to a specific topic and waits for the response.
+
+The ResponseTopic property is used to specify the topic to which the response should be sent."""
+
+import time
+import uuid
+from typing import Callable
+
+from ohmqtt.client import Client
+
+from ohmqtt.message import MQTTMessage
+
+
+RPCCallback = Callable[[bytes], None]
+
+
+class RPCCaller:
+    """A simple RPC caller."""
+    def __init__(self, client: Client) -> None:
+        self.client = client
+        self.callbacks: dict[str, RPCCallback] = {}
+
+    def send_request(self, payload: bytes, callback: RPCCallback) -> None:
+        # Expect the response to be sent to a unique topic.
+        unique_id = str(uuid.uuid4())
+        response_topic = f"ohmqtt/examples/rpc/response/{unique_id}"
+        # Store the callback for this request.
+        self.callbacks[unique_id] = callback
+        # Subscribe to the response topic.
+        self.client.subscribe(response_topic, qos=2, callback=self.handle_response)
+        # Publish the request with the necessary properties.
+        self.client.publish(
+            "ohmqtt/examples/rpc/request",
+            payload,
+            qos=2,
+            properties={"ResponseTopic": response_topic},
+        ).wait_for_ack()
+        print(f"Sent RPC request with response topic: {response_topic}")
+
+    def handle_response(self, client: Client, msg: MQTTMessage) -> None:
+        """Handle incoming RPC responses."""
+        try:
+            callback = self.callbacks.pop(msg.topic.split("/")[-1])
+        except KeyError:
+            print("Couldn't find callback for response")
+            return
+        else:
+            # Call the callback with the response payload.
+            callback(msg.payload)
+        finally:
+            # Unsubscribe from the response topic.
+            client.unsubscribe(msg.topic)
+
+
+def main() -> None:
+    with Client() as client:
+        rpc_caller = RPCCaller(client)
+        client.connect("localhost", 1883)
+        client.wait_for_connect(timeout=5.0)
+
+        while True:
+            try:
+                payload = input("*** Enter something fun (or 'exit' to quit): ")
+                if payload.lower() == "exit":
+                    break
+                rpc_caller.send_request(payload.encode(), lambda response: print(f"Received response: {response.decode()}"))
+                time.sleep(1)  # Give some time for the response to be received.
+            except (EOFError, KeyboardInterrupt):
+                print("\n*** Shutting down RPC caller...")
+                break
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/rpc_server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""This example demonstrates a simple RPC server.
+
+It listens for incoming RPC requests on a specific topic and responds with the result of the RPC call.
+
+The ResponseTopic property is used by the requestor to specify the topic to which the response should be sent.
+
+See the "rpc_caller" example for the request side of this RPC implementation."""
+
+import signal
+
+from ohmqtt.client import Client
+from ohmqtt.message import MQTTMessage
+from ohmqtt.property import MQTTPropertyDict
+
+
+class RPCServer:
+    """A simple stateless RPC server."""
+    def handle_request(self, client: Client, msg: MQTTMessage) -> None:
+        """Handle incoming RPC requests."""
+        print(f"*** Received RPC request: {str(msg)}")
+        # Find the response topic in the message properties.
+        try:
+            response_topic = msg.properties["ResponseTopic"]
+        except KeyError:
+            print("Request was missing required property: ResponseTopic")
+            return
+        # If the request includes correlation data, send it back in the response.
+        response_props: MQTTPropertyDict = {}
+        if "CorrelationData" in msg.properties:
+            response_props["CorrelationData"] = msg.properties["CorrelationData"]
+        # Simulate some processing.
+        response = f"This is a good day for {msg.payload.decode()}"
+        # Send the response back to the specified topic.
+        client.publish(response_topic, response.encode(), qos=2, properties=response_props)
+
+
+def main() -> None:
+    rpc_server = RPCServer()
+    with Client() as client:
+        client.connect("localhost", 1883)
+        client.wait_for_connect(timeout=5.0)
+        client.subscribe("ohmqtt/examples/rpc/request", qos=2, callback=rpc_server.handle_request)
+
+        print("*** Waiting for RPC requests...")
+        try:
+            signal.pause()  # Wait indefinitely for incoming messages.
+        except KeyboardInterrupt:
+            print("\n*** Shutting down RPC server...")
+
+
+if __name__ == "__main__":
+    main()
