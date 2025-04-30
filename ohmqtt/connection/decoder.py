ca72b29758ecad0ce53d2f7a8f1fb2869d892b79
Matt Vollrath <matt@endpointdev.com>
2025-04-30 01:44:03 -0400

    Refactor IncrementalDecoder
    for less complexipy.

--- ohmqtt/connection/decoder.py
+++ ohmqtt/connection/decoder.py
@@ -4,9 +4,12 @@ import ssl
 from typing import NamedTuple, Final
 
 from ..error import MQTTError
+from ..logger import get_logger
 from ..mqtt_spec import MQTTReasonCode
 from ..packet import decode_packet_from_parts, MQTTPacket
 from ..serialization import MAX_VARINT
+
+logger: Final = get_logger("connection.decoder")
 
 
 class VarintDecodeResult(NamedTuple):
@@ -31,62 +34,98 @@ class WantRead(Exception):
     pass
 
 
-def decode_varint_from_socket(sock: socket.socket | ssl.SSLSocket, partial: int, mult: int) -> VarintDecodeResult:
-    """Incrementally decode a variable length integer from a socket.
-
-    Returns -1 if there is not enough data to decode the varint."""
-    result = partial
-    sz = 0
-    while True:
-        try:
-            data = sock.recv(1)
-        except (BlockingIOError, ssl.SSLWantReadError):
-            return VarintDecodeResult(result, mult, False)
-        if not data:
-            raise ClosedSocketError("Socket closed")
-        byte = data[0]
-        sz += 1
-        result += byte % 0x80 * mult
-        if result > MAX_VARINT:
-            raise MQTTError("Varint overflow", MQTTReasonCode.MalformedPacket)
-        if byte < 0x80:
-            return VarintDecodeResult(result, mult, True)
-        mult *= 0x80
-
-
 @dataclass(slots=True)
+class DecoderState:
+    """State of the decoder.
+
+    Attributes:
+        head: The first byte of the packet.
+        length: Variable integer decoding state of the packet length.
+        data: The remaining data of the packet.
+    """
+    head: int = field(default=-1, init=False)
+    length: VarintDecodeResult = field(default=InitVarintDecodeState, init=False)
+    data: bytearray = field(init=False, default_factory=bytearray)
+
+
 class IncrementalDecoder:
     """Incremental decoder for MQTT messages coming in from a socket."""
-    _partial_head: int = field(default=-1, init=False)
-    _partial_length: VarintDecodeResult = field(default=InitVarintDecodeState, init=False)
-    _partial_data: bytearray = field(init=False, default_factory=bytearray)
+    __slots__ = ("_state",)
+
+    def __init__(self) -> None:
+        self._state: DecoderState = DecoderState()
+        self.reset()
 
     def reset(self) -> None:
         """Reset the decoder state."""
-        self._partial_head = -1
-        self._partial_length = InitVarintDecodeState
-        self._partial_data.clear()
+        self._state.head = -1
+        self._state.length = InitVarintDecodeState
+        self._state.data.clear()
+
+    def _recv_one_byte(self, sock: socket.socket | ssl.SSLSocket) -> int:
+        """Receive one byte from the socket.
+
+        Raises WantRead if the socket is not ready for reading.
+
+        Raises ClosedSocketError if the socket is closed."""
+        try:
+            data = sock.recv(1)
+        except (BlockingIOError, ssl.SSLWantReadError):
+            raise WantRead("Socket not ready for reading")
+        if not data:
+            raise ClosedSocketError("Socket closed")
+        return data[0]
+
+    def _extract_head(self, sock: socket.socket | ssl.SSLSocket) -> None:
+        """Extract the head byte of the next packet from the socket, if needed."""
+        if self._state.head != -1:
+            return
+        self._state.head = self._recv_one_byte(sock)
+
+    def _extract_length(self, sock: socket.socket | ssl.SSLSocket) -> None:
+        """Incrementally decode a variable length integer from a socket, if needed.
+
+        Raises WantRead if the socket is not ready for reading."""
+        # See ohmqtt.serialization.decode_varint for a cleaner implementation.
+        assert self._state.head != -1  # We shouldn't be here unless we have a head byte.
+        if self._state.length.complete:
+            return
+        result = self._state.length.value
+        mult = self._state.length.multiplier
+        sz = 0
+        while True:
+            try:
+                byte = self._recv_one_byte(sock)
+            except WantRead:
+                # Not done yet, the socket is neither closed nor ready for reading.
+                # Save the partial state and return.
+                self._state.length = VarintDecodeResult(result, mult, False)
+                raise
+            sz += 1
+            result += byte % 0x80 * mult
+            if result > MAX_VARINT:
+                raise MQTTError("Varint overflow", MQTTReasonCode.MalformedPacket)
+            if byte < 0x80:
+                # We have the complete varint.
+                self._state.length = VarintDecodeResult(result, mult, True)
+                return
+            mult *= 0x80
+
+    def _extract_data(self, sock: socket.socket | ssl.SSLSocket) -> None:
+        """Extract all data after the packet length from the socket, if needed."""
+        assert self._state.length.complete  # We shouldn't be here unless we have a complete length.
+        while len(self._state.data) < self._state.length.value:
+            data = sock.recv(self._state.length.value - len(self._state.data))
+            if not data:
+                raise ClosedSocketError("Socket closed")
+            self._state.data.extend(data)
 
     def decode(self, sock: socket.socket | ssl.SSLSocket) -> MQTTPacket | None:
-        """Decode a packet from the socket."""
+        """Decode a single packet straight from the socket."""
         try:
-            if self._partial_head == -1:
-                partial_head = sock.recv(1)
-                if not partial_head:
-                    raise ClosedSocketError("Socket closed")
-                self._partial_head = partial_head[0]
-            if not self._partial_length.complete:
-                self._partial_length = decode_varint_from_socket(sock, self._partial_length.value, self._partial_length.multiplier)
-                if not self._partial_length.complete:
-                    # If the socket didn't have enough data for us, we need to wait for more.
-                    return None
-
-            while len(self._partial_data) < self._partial_length.value:
-                # Read the rest of the packet.
-                data = sock.recv(self._partial_length.value - len(self._partial_data))
-                if not data:
-                    raise ClosedSocketError("Socket closed")
-                self._partial_data.extend(data)
+            self._extract_head(sock)
+            self._extract_length(sock)
+            self._extract_data(sock)
         except (BlockingIOError, ssl.SSLWantReadError, WantRead):
             # If the socket is open but doesn't have enough data for us, we need to wait for more.
             return None
@@ -94,9 +133,9 @@ class IncrementalDecoder:
             raise ClosedSocketError("Socket closed") from exc
 
         # We have a complete packet, decode it and clear the read buffer.
-        packet_data = memoryview(self._partial_data)
+        packet_head = self._state.head
+        packet_data = memoryview(self._state.data)
         packet_data.toreadonly()
-        packet_head = self._partial_head
         try:
             return decode_packet_from_parts(packet_head, packet_data)
         finally:
