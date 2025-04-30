0fb906b0072801cde3ef36c7801c448852f280f9
Matt Vollrath <matt@endpointdev.com>
2025-04-30 00:32:51 -0400

    Run complexipy

--- noxfile.py
+++ noxfile.py
@@ -7,8 +7,20 @@ py_versions = ("3.10", "3.11", "3.12", "3.13")
 @nox.session(python=py_versions)
 def tests(session):
     session.install(".")
-    session.install("cryptography", "mypy", "pytest", "pytest-cov", "pytest-mock", "pyyaml", "ruff", "typos")
+    session.install(
+        "complexipy",
+        "cryptography",
+        "mypy",
+        "pytest",
+        "pytest-cov",
+        "pytest-mock",
+        "pyyaml",
+        "ruff",
+        "typos",
+    )
     session.run("ruff", "check")
     session.run("typos")
     session.run("mypy")
+    session.run("complexipy", "-d", "low", "ohmqtt", "examples")
     session.run("pytest", "--cov-report=term-missing")
+
