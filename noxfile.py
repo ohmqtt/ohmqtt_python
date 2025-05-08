c2af8081f679fa4b9db28245fdd75df3bda9dd0f
Matt Vollrath <matt@endpointdev.com>
2025-05-07 22:30:24 -0400

    Skip complexipy on Windows
    Broken due to default encoding.
    
    <https://github.com/rohaquinlop/complexipy/issues/67>

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 import nox
+import sys
 
 nox.options.default_venv_backend = "uv|virtualenv"
 py_versions = ("3.10", "3.11", "3.12", "3.13")
@@ -7,21 +8,12 @@ py_versions = ("3.10", "3.11", "3.12", "3.13")
 @nox.session(python=py_versions)
 def tests(session):
     session.install(".")
-    session.install(
-        "complexipy",
-        "cryptography",
-        "mypy",
-        "pytest",
-        "pytest-cov",
-        "pytest-mock",
-        "pyyaml",
-        "ruff",
-        "typing_extensions",
-        "typos",
-    )
+    session.install("--group", "dev")
     session.run("ruff", "check")
     session.run("typos")
     session.run("mypy")
-    session.run("complexipy", "-d", "low", "ohmqtt", "examples")
+    if sys.platform != "win32":
+        session.run("complexipy", "-d", "low", "ohmqtt", "examples")
+    else:
+        session.log("Skipping complexipy check on Windows: https://github.com/rohaquinlop/complexipy/issues/67")
     session.run("pytest", "--cov-report=term-missing")
-
