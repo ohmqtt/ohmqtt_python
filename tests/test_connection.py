36dacdfcf2432cec6a5dd10a1fe76be2bf014594
Matt Vollrath <matt@endpointdev.com>
2025-05-12 19:40:28 -0400

    Annotations for test_connection

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,4 +1,8 @@
+from typing import Callable
+from unittest.mock import Mock
+
 import pytest
+from pytest_mock import MockerFixture
 
 from ohmqtt.connection import (
     Address,
@@ -24,52 +28,57 @@ from ohmqtt.packet import (
 
 
 @pytest.fixture
-def mock_handlers(mocker):
-    return mocker.Mock(spec=MessageHandlers)
+def mock_handlers(mocker: MockerFixture) -> Mock:
+    return mocker.Mock(spec=MessageHandlers)  # type: ignore[no-any-return]
 
 
 @pytest.fixture
-def mock_fsm(mocker):
+def mock_fsm(mocker: MockerFixture) -> Mock:
     mock_fsm = mocker.Mock(spec=FSM)
     mock_fsm.lock = mocker.MagicMock()
     mock_fsm.lock.__enter__.return_value = mock_fsm.lock
     mocker.patch("ohmqtt.connection.FSM", return_value=mock_fsm)
-    return mock_fsm
+    return mock_fsm  # type: ignore[no-any-return]
 
 
-def test_connection_handle_packet(mocker):
+def test_connection_handle_packet(mocker: MockerFixture) -> None:
+    fake_handlers = [
+        mocker.Mock(spec=Callable[[MQTTPublishPacket], None]),
+        mocker.Mock(spec=Callable[[MQTTPublishPacket], None]),
+    ]
+
     handlers = MessageHandlers()
     with handlers:
-        handlers.register(MQTTPublishPacket, mocker.Mock())
-        handlers.register(MQTTPublishPacket, mocker.Mock())
+        for fake_handler in fake_handlers:
+            handlers.register(MQTTPublishPacket, fake_handler)
     connection = Connection(handlers)
     packet = MQTTPublishPacket()
 
     connection.handle_packet(packet)
-    for handler in handlers.get_handlers(MQTTPublishPacket):
+    for handler in fake_handlers:
         handler.assert_called_once_with(packet)
         handler.reset_mock()
 
     # At least one exception should be raised if any handler raises an exception
-    handlers.get_handlers(MQTTPublishPacket)[0].side_effect = Exception("TEST")
+    fake_handlers[0].side_effect = Exception("TEST")
     with pytest.raises(Exception):
         connection.handle_packet(packet)
     # All handlers are still called
-    for handler in handlers.get_handlers(MQTTPublishPacket):
+    for handler in fake_handlers:
         handler.assert_called_once_with(packet)
         handler.reset_mock()
 
     # MQTTError has priority over other exceptions
-    handlers.get_handlers(MQTTPublishPacket)[1].side_effect = MQTTError("TEST", MQTTReasonCode.UnspecifiedError)
+    fake_handlers[1].side_effect = MQTTError("TEST", MQTTReasonCode.UnspecifiedError)
     with pytest.raises(MQTTError):
         connection.handle_packet(packet)
     # All handlers are still called
-    for handler in handlers.get_handlers(MQTTPublishPacket):
+    for handler in fake_handlers:
         handler.assert_called_once_with(packet)
         handler.reset_mock()
 
 
-def test_connection_can_send(mock_fsm, mock_handlers):
+def test_connection_can_send(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     mock_fsm.get_state.return_value = ClosedState
     assert not connection.can_send()
@@ -77,7 +86,7 @@ def test_connection_can_send(mock_fsm, mock_handlers):
     assert connection.can_send()
 
 
-def test_connection_send(mocker, mock_fsm, mock_handlers):
+def test_connection_send(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     mock_fsm.get_state.return_value = ConnectedState
     mock_fsm.env.write_buffer = bytearray()
@@ -89,7 +98,7 @@ def test_connection_send(mocker, mock_fsm, mock_handlers):
         connection.send(packet)
 
 
-def test_connection_connect(mocker, mock_fsm, mock_handlers):
+def test_connection_connect(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     params = ConnectParams(address=Address("test_address"))
     connection.connect(params)
@@ -98,21 +107,21 @@ def test_connection_connect(mocker, mock_fsm, mock_handlers):
     mock_fsm.selector.interrupt.assert_called_once_with()
 
 
-def test_connection_disconnect(mocker, mock_fsm, mock_handlers):
+def test_connection_disconnect(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     connection.disconnect()
     mock_fsm.request_state.assert_called_once_with(ClosingState)
     mock_fsm.selector.interrupt.assert_called_once_with()
 
 
-def test_connection_shutdown(mocker, mock_fsm, mock_handlers):
+def test_connection_shutdown(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     connection.shutdown()
     mock_fsm.request_state.assert_called_once_with(ShutdownState)
     mock_fsm.selector.interrupt.assert_called_once_with()
 
 
-def test_connection_is_connected(mocker, mock_fsm, mock_handlers):
+def test_connection_is_connected(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     mock_fsm.get_state.return_value = ConnectedState
     assert connection.is_connected() is True
@@ -120,7 +129,7 @@ def test_connection_is_connected(mocker, mock_fsm, mock_handlers):
     assert connection.is_connected() is False
 
 
-def test_connection_wait_for_connect(mocker, mock_fsm, mock_handlers):
+def test_connection_wait_for_connect(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     mock_fsm.wait_for_state.return_value = True
     assert connection.wait_for_connect(0.1) is True
@@ -129,7 +138,7 @@ def test_connection_wait_for_connect(mocker, mock_fsm, mock_handlers):
     assert connection.wait_for_connect(0.1) is False
 
 
-def test_connection_wait_for_disconnect(mocker, mock_fsm, mock_handlers):
+def test_connection_wait_for_disconnect(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     mock_fsm.wait_for_state.return_value = True
     assert connection.wait_for_disconnect(0.1) is True
@@ -138,7 +147,7 @@ def test_connection_wait_for_disconnect(mocker, mock_fsm, mock_handlers):
     assert connection.wait_for_disconnect(0.1) is False
 
 
-def test_connection_wait_for_shutdown(mocker, mock_fsm, mock_handlers):
+def test_connection_wait_for_shutdown(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     mock_fsm.wait_for_state.return_value = True
     assert connection.wait_for_shutdown(0.1) is True
@@ -147,7 +156,7 @@ def test_connection_wait_for_shutdown(mocker, mock_fsm, mock_handlers):
     assert connection.wait_for_shutdown(0.1) is False
 
 
-def test_connection_loop_once(mocker, mock_fsm, mock_handlers):
+def test_connection_loop_once(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     connection.loop_once(0.1)
     mock_fsm.loop_once.assert_called_once_with(0.1)
@@ -156,13 +165,13 @@ def test_connection_loop_once(mocker, mock_fsm, mock_handlers):
     mock_fsm.loop_once.assert_called_with(0.0)
 
 
-def test_connection_loop_forever(mocker, mock_fsm, mock_handlers):
+def test_connection_loop_forever(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     connection.loop_forever()
     mock_fsm.loop_until_state.assert_called_once_with((ShutdownState,))
 
 
-def test_connection_loop_until_connected(mocker, mock_fsm, mock_handlers):
+def test_connection_loop_until_connected(mock_fsm: Mock, mock_handlers: Mock) -> None:
     connection = Connection(mock_handlers)
     mock_fsm.loop_until_state.return_value = True
     assert connection.loop_until_connected(0.1) is True
