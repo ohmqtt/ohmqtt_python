33afadfb1d30c8aaba3aa542d026bb36f7812be1
Matt Vollrath <matt@endpointdev.com>
2025-05-12 23:44:24 -0400

    Complexipy the tests

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,7 @@ def tests(session):
     session.run("typos")
     session.run("mypy")
     if sys.platform != "win32":
-        session.run("complexipy", "-d", "low", "ohmqtt", "examples")
+        session.run("complexipy", "-d", "low", "ohmqtt", "examples", "tests")
     else:
         session.log("Skipping complexipy check on Windows: https://github.com/rohaquinlop/complexipy/issues/67")
     session.run("pytest", "--cov-report=term-missing")
