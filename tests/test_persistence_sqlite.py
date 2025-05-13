1dcd432e837f47ca203e157318ceeade570e43fd
Matt Vollrath <matt@endpointdev.com>
2025-05-12 21:43:45 -0400

    Annotations for test_persistence_sqlite

--- tests/test_persistence_sqlite.py
+++ tests/test_persistence_sqlite.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import tempfile
+from typing import Generator
 
 import pytest
 
@@ -10,13 +11,13 @@ from ohmqtt.topic_alias import AliasPolicy
 
 
 @pytest.fixture
-def tempdbpath():
+def tempdbpath() -> Generator[str, None, None]:
     """Fixture to create a temporary directory for testing."""
     with tempfile.TemporaryDirectory() as tempdir:
-        yield Path(tempdir) / "test.db"
+        yield str(Path(tempdir) / "test.db")
 
 
-def test_persistence_sqlite_happy_path_qos1():
+def test_persistence_sqlite_happy_path_qos1() -> None:
     """Test the SQLitePersistence class with a happy path scenario qos=1."""
     persistence = SQLitePersistence(":memory:")
     persistence.open("test_client")
@@ -59,7 +60,7 @@ def test_persistence_sqlite_happy_path_qos1():
     assert len(persistence) == 0
 
 
-def test_persistence_sqlite_happy_path_qos2():
+def test_persistence_sqlite_happy_path_qos2() -> None:
     """Test the SQLitePersistence class with a happy path scenario qos=2."""
     persistence = SQLitePersistence(":memory:")
     persistence.open("test_client")
@@ -79,7 +80,7 @@ def test_persistence_sqlite_happy_path_qos2():
     # Retrieve the PUBLISH from the store.
     message_ids = persistence.get(10)
     assert len(message_ids) == 1
-    expected_packet = MQTTPublishPacket(
+    expected_publish = MQTTPublishPacket(
         packet_id=1,
         topic="test/topic",
         payload=b"test payload",
@@ -90,7 +91,7 @@ def test_persistence_sqlite_happy_path_qos2():
 
     # Render the message, marking it as inflight.
     rendered = persistence.render(message_ids[0])
-    assert rendered.packet == expected_packet
+    assert rendered.packet == expected_publish
     assert rendered.alias_policy == AliasPolicy.TRY
     assert len(persistence) == 1
 
@@ -104,11 +105,11 @@ def test_persistence_sqlite_happy_path_qos2():
     # Retrieve the PUBREL from the store.
     message_ids = persistence.get(10)
     assert len(message_ids) == 1
-    expected_packet = MQTTPubRelPacket(packet_id=1)
+    expected_pubrel = MQTTPubRelPacket(packet_id=1)
 
     # Render the message, marking it as inflight.
     rendered = persistence.render(message_ids[0])
-    assert rendered.packet == expected_packet
+    assert rendered.packet == expected_pubrel
     assert rendered.alias_policy == AliasPolicy.NEVER
     assert len(persistence) == 1
 
@@ -121,7 +122,7 @@ def test_persistence_sqlite_happy_path_qos2():
 
 
 @pytest.mark.parametrize("db_fast", [True, False])
-def test_persistence_sqlite_open(db_fast, tempdbpath):
+def test_persistence_sqlite_open(db_fast: bool, tempdbpath: str) -> None:
     """Test the SQLitePersistence class with a resume scenario."""
     persistence = SQLitePersistence(tempdbpath, db_fast=db_fast)
     persistence.open("test_client")
@@ -172,7 +173,7 @@ def test_persistence_sqlite_open(db_fast, tempdbpath):
     assert len(persistence) == 0
 
 
-def test_persistence_sqlite_properties():
+def test_persistence_sqlite_properties() -> None:
     persistence = SQLitePersistence(":memory:")
     persistence.open("test_client")
 
@@ -207,7 +208,7 @@ def test_persistence_sqlite_properties():
     assert rendered.packet == packet
 
 
-def test_persistence_sqlite_loose_alias():
+def test_persistence_sqlite_loose_alias() -> None:
     """We should not be able to add a message with an alias policy of ALWAYS."""
     persistence = SQLitePersistence(":memory:")
     persistence.open("test_client")
