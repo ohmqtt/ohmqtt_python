62a4692d7cc1af462d4e5430909e9dcf9ddca5f2
Matt Vollrath <matt@endpointdev.com>
2025-06-20 11:35:02 -0700

    Run complexipy on Windows

--- noxfile.py
+++ noxfile.py
@@ -7,13 +7,11 @@ py_versions = ("3.10", "3.11", "3.12", "3.13")
 
 @nox.session(python=py_versions)
 def tests(session):
+    complexipy_env = {"PYTHONUTF8": "1"} if sys.platform.startswith("win") else None
     session.install(".")
     session.install("--group", "dev")
     session.run("ruff", "check")
     session.run("typos")
     session.run("mypy")
-    if sys.platform != "win32":
-        session.run("complexipy", "-d", "low", "ohmqtt", "examples", "tests")
-    else:
-        session.log("Skipping complexipy check on Windows: https://github.com/rohaquinlop/complexipy/issues/67")
+    session.run("complexipy", "-d", "low", "ohmqtt", "examples", "tests", env=complexipy_env)
     session.run("pytest", "--cov-report=term-missing")
