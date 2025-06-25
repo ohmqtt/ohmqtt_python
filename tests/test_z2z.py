aff725415abfa86c36620ac681af4f8788de99ff
Matt Vollrath <matt@endpointdev.com>
2025-06-24 22:05:59 -0700

    Refactor E2E happy path test

--- tests/test_z2z.py
+++ tests/test_z2z.py
@@ -3,7 +3,9 @@
 from __future__ import annotations
 
 import time
-from typing import Final
+from typing import Final, Iterator
+
+import pytest
 
 from .util.fake_broker import FakeBroker
 from ohmqtt.client import Client
@@ -22,77 +24,91 @@ from ohmqtt.packet import (
 logger: Final = get_logger("tests.test_z2z")
 
 
-def test_z2z_happy_path() -> None:
+@pytest.fixture
+def client() -> Client:
+    client = Client()
+    client.start_loop()
+    return client
+
+
+@pytest.fixture
+def broker() -> Iterator[FakeBroker]:
+    with FakeBroker() as broker:
+        yield broker
+
+
+def test_z2z_happy_path(client: Client, broker: FakeBroker) -> None:
     client_received = []
     def callback(client: Client, packet: MQTTPacket) -> None:
         client_received.append(packet)
 
-    with FakeBroker() as broker:
-        with Client() as client:
-            client.connect(
-                address=f"localhost:{broker.port}",
-                client_id="test_client",
-                clean_start=True,
-            )
-            client.wait_for_connect(timeout=0.25)
-            assert client.is_connected()
-            assert broker.received.pop(0) == MQTTConnectPacket(
-                client_id="test_client",
-                clean_start=True,
-            )
+    client.connect(
+        address=f"localhost:{broker.port}",
+        client_id="test_client",
+        clean_start=True,
+    )
+    client.wait_for_connect(timeout=0.25)
+    assert client.is_connected()
+    assert broker.received.pop(0) == MQTTConnectPacket(
+        client_id="test_client",
+        clean_start=True,
+    )
 
-            # SUBSCRIBE
-            sub_handle = client.subscribe("test/topic", callback)
-            assert sub_handle is not None
-            sub_handle.wait_for_ack(timeout=0.25)
-            logger.info("SUBSCRIBE acked")
-            time.sleep(0.01)
-            assert broker.received.pop(0) == MQTTSubscribePacket(
-                topics=[("test/topic", 2)],
-                packet_id=1,
-            )
+    # SUBSCRIBE
+    sub_handle = client.subscribe("test/topic", callback)
+    assert sub_handle is not None
+    sub_handle.wait_for_ack(timeout=0.25)
+    logger.info("SUBSCRIBE acked")
+    time.sleep(0.01)
+    assert broker.received.pop(0) == MQTTSubscribePacket(
+        topics=[("test/topic", 2)],
+        packet_id=1,
+    )
 
-            # PUBLISH QoS 1
-            pub_handle = client.publish("test/topic", b"coconut", qos=1)
-            pub_handle.wait_for_ack(timeout=0.25)
-            logger.info("QoS1 acked")
-            time.sleep(0.01)
-            assert broker.received.pop(0) == client_received.pop(0) == MQTTPublishPacket(
-                topic="test/topic",
-                payload=b"coconut",
-                qos=1,
-                packet_id=1,
-            )
-            assert broker.received.pop(0) == MQTTPubAckPacket(packet_id=1)
+    # PUBLISH QoS 1
+    for n in range(1, 50):
+        pub_handle = client.publish("test/topic", b"coconut", qos=1)
+        pub_handle.wait_for_ack(timeout=0.25)
+        logger.info("QoS1 acked")
+        time.sleep(0.01)
+        assert broker.received.pop(0) == client_received.pop(0) == MQTTPublishPacket(
+            topic="test/topic",
+            payload=b"coconut",
+            qos=1,
+            packet_id=n,
+        )
+        assert broker.received.pop(0) == MQTTPubAckPacket(packet_id=n)
 
-            # UNSUBSCRIBE
-            unsub_handle = client.unsubscribe("test/topic", callback)
-            assert unsub_handle is not None
-            unsub_handle.wait_for_ack(timeout=0.25)
-            logger.info("UNSUBSCRIBE acked")
-            time.sleep(0.01)
-            assert broker.received.pop(0) == MQTTUnsubscribePacket(
-                topics=["test/topic"],
-                packet_id=1,
-            )
+    # UNSUBSCRIBE
+    unsub_handle = client.unsubscribe("test/topic", callback)
+    assert unsub_handle is not None
+    unsub_handle.wait_for_ack(timeout=0.25)
+    logger.info("UNSUBSCRIBE acked")
+    time.sleep(0.01)
+    assert broker.received.pop(0) == MQTTUnsubscribePacket(
+        topics=["test/topic"],
+        packet_id=1,
+    )
 
-            # PUBLISH QoS 2
-            pub_handle = client.publish("test/topic", b"pineapple", qos=2)
-            pub_handle.wait_for_ack(timeout=0.25)
-            logger.info("QoS2 acked")
-            time.sleep(0.01)
-            assert broker.received.pop(0) == MQTTPublishPacket(
-                topic="test/topic",
-                payload=b"pineapple",
-                qos=2,
-                packet_id=2,
-            )
-            assert broker.received.pop(0) == MQTTPubRelPacket(packet_id=2)
+    # PUBLISH QoS 2
+    for n in range(50, 100):
+        pub_handle = client.publish("test/topic", b"pineapple", qos=2)
+        pub_handle.wait_for_ack(timeout=0.25)
+        logger.info("QoS2 acked")
+        time.sleep(0.01)
+        assert broker.received.pop(0) == MQTTPublishPacket(
+            topic="test/topic",
+            payload=b"pineapple",
+            qos=2,
+            packet_id=n,
+        )
+        assert broker.received.pop(0) == MQTTPubRelPacket(packet_id=n)
 
-            # DISCONNECT
-            client.disconnect()
-            client.wait_for_disconnect(timeout=0.25)
-            time.sleep(0.01)
-            assert broker.received.pop(0) == MQTTDisconnectPacket()
+    # DISCONNECT
+    client.disconnect()
+    client.wait_for_disconnect(timeout=0.25)
+    time.sleep(0.01)
+    assert broker.received.pop(0) == MQTTDisconnectPacket()
 
-        client.wait_for_shutdown(timeout=0.25)
+    client.shutdown()
+    client.wait_for_shutdown(timeout=0.25)
